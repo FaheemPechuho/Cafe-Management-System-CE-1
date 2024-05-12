6fbab41a53c3af266e020ce073bb52812e8c1bf0
FaheemPechuho <faheempechuho56@gmal.com>
2024-05-12 19:45:26 +0500

    Add project files.

---
+++ UI Forms/Customer/CustomerFeedbackForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Cafe_Management_System_CE_1.UI_Forms
+{
+    partial class CustomerFeedbackForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomerFeedbackForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.feedbackButton = new System.Windows.Forms.Button();
+            this.promotionsButton = new System.Windows.Forms.Button();
+            this.orderSummary = new System.Windows.Forms.Button();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.commentsTextbox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.star1 = new System.Windows.Forms.RadioButton();
+            this.star2 = new System.Windows.Forms.RadioButton();
+            this.star3 = new System.Windows.Forms.RadioButton();
+            this.star4 = new System.Windows.Forms.RadioButton();
+            this.star5 = new System.Windows.Forms.RadioButton();
+            this.submitReviewButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.logoutButton);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.feedbackButton);
+            this.panel1.Controls.Add(this.promotionsButton);
+            this.panel1.Controls.Add(this.orderSummary);
+            this.panel1.Controls.Add(this.menuButton);
+            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(268, 505);
+            this.panel1.TabIndex = 0;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.BackColor = System.Drawing.Color.DarkRed;
+            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.logoutButton.ForeColor = System.Drawing.Color.Beige;
+            this.logoutButton.Location = new System.Drawing.Point(0, 386);
+            this.logoutButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(274, 40);
+            this.logoutButton.TabIndex = 11;
+            this.logoutButton.Text = "Log Out";
+            this.logoutButton.UseVisualStyleBackColor = false;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 173);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // feedbackButton
+            // 
+            this.feedbackButton.BackColor = System.Drawing.Color.DarkSalmon;
+            this.feedbackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.feedbackButton.ForeColor = System.Drawing.Color.Beige;
+            this.feedbackButton.Location = new System.Drawing.Point(0, 334);
+            this.feedbackButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.feedbackButton.Name = "feedbackButton";
+            this.feedbackButton.Size = new System.Drawing.Size(274, 40);
+            this.feedbackButton.TabIndex = 3;
+            this.feedbackButton.Text = "Feedbacks";
+            this.feedbackButton.UseVisualStyleBackColor = false;
+            this.feedbackButton.Click += new System.EventHandler(this.feedbackButton_Click);
+            // 
+            // promotionsButton
+            // 
+            this.promotionsButton.BackColor = System.Drawing.Color.DarkRed;
+            this.promotionsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.promotionsButton.ForeColor = System.Drawing.Color.Beige;
+            this.promotionsButton.Location = new System.Drawing.Point(0, 283);
+            this.promotionsButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.promotionsButton.Name = "promotionsButton";
+            this.promotionsButton.Size = new System.Drawing.Size(274, 40);
+            this.promotionsButton.TabIndex = 2;
+            this.promotionsButton.Text = "Promotions";
+            this.promotionsButton.UseVisualStyleBackColor = false;
+            this.promotionsButton.Click += new System.EventHandler(this.promotionsButton_Click);
+            // 
+            // orderSummary
+            // 
+            this.orderSummary.BackColor = System.Drawing.Color.DarkRed;
+            this.orderSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.orderSummary.ForeColor = System.Drawing.Color.Beige;
+            this.orderSummary.Location = new System.Drawing.Point(0, 232);
+            this.orderSummary.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.orderSummary.Name = "orderSummary";
+            this.orderSummary.Size = new System.Drawing.Size(274, 40);
+            this.orderSummary.TabIndex = 1;
+            this.orderSummary.Text = "Order Summary";
+            this.orderSummary.UseVisualStyleBackColor = false;
+            this.orderSummary.Click += new System.EventHandler(this.orderSummary_Click);
+            // 
+            // menuButton
+            // 
+            this.menuButton.BackColor = System.Drawing.Color.DarkRed;
+            this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.menuButton.ForeColor = System.Drawing.Color.Beige;
+            this.menuButton.Location = new System.Drawing.Point(0, 181);
+            this.menuButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(274, 40);
+            this.menuButton.TabIndex = 0;
+            this.menuButton.Text = "Place Order";
+            this.menuButton.UseVisualStyleBackColor = false;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(446, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Give us Feedback!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(309, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Comments";
+            // 
+            // commentsTextbox
+            // 
+            this.commentsTextbox.Location = new System.Drawing.Point(426, 296);
+            this.commentsTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.commentsTextbox.Name = "commentsTextbox";
+            this.commentsTextbox.Size = new System.Drawing.Size(401, 131);
+            this.commentsTextbox.TabIndex = 4;
+            this.commentsTextbox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(309, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rate us!";
+            // 
+            // star1
+            // 
+            this.star1.AutoSize = true;
+            this.star1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.star1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.star1.Location = new System.Drawing.Point(426, 86);
+            this.star1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.star1.Name = "star1";
+            this.star1.Size = new System.Drawing.Size(44, 33);
+            this.star1.TabIndex = 6;
+            this.star1.TabStop = true;
+            this.star1.Text = "*";
+            this.star1.UseVisualStyleBackColor = true;
+            // 
+            // star2
+            // 
+            this.star2.AutoSize = true;
+            this.star2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.star2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.star2.Location = new System.Drawing.Point(426, 123);
+            this.star2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.star2.Name = "star2";
+            this.star2.Size = new System.Drawing.Size(54, 33);
+            this.star2.TabIndex = 7;
+            this.star2.TabStop = true;
+            this.star2.Text = "**";
+            this.star2.UseVisualStyleBackColor = true;
+            // 
+            // star3
+            // 
+            this.star3.AutoSize = true;
+            this.star3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.star3.ForeColor = System.Drawing.Color.Goldenrod;
+            this.star3.Location = new System.Drawing.Point(426, 160);
+            this.star3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.star3.Name = "star3";
+            this.star3.Size = new System.Drawing.Size(64, 33);
+            this.star3.TabIndex = 8;
+            this.star3.TabStop = true;
+            this.star3.Text = "***";
+            this.star3.UseVisualStyleBackColor = true;
+            // 
+            // star4
+            // 
+            this.star4.AutoSize = true;
+            this.star4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.star4.ForeColor = System.Drawing.Color.Goldenrod;
+            this.star4.Location = new System.Drawing.Point(426, 197);
+            this.star4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.star4.Name = "star4";
+            this.star4.Size = new System.Drawing.Size(74, 33);
+            this.star4.TabIndex = 9;
+            this.star4.TabStop = true;
+            this.star4.Text = "****";
+            this.star4.UseVisualStyleBackColor = true;
+            // 
+            // star5
+            // 
+            this.star5.AutoSize = true;
+            this.star5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.star5.ForeColor = System.Drawing.Color.Goldenrod;
+            this.star5.Location = new System.Drawing.Point(426, 234);
+            this.star5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.star5.Name = "star5";
+            this.star5.Size = new System.Drawing.Size(84, 33);
+            this.star5.TabIndex = 10;
+            this.star5.TabStop = true;
+            this.star5.Text = "*****";
+            this.star5.UseVisualStyleBackColor = true;
+            // 
+            // submitReviewButton
+            // 
+            this.submitReviewButton.BackColor = System.Drawing.Color.DarkRed;
+            this.submitReviewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.submitReviewButton.ForeColor = System.Drawing.Color.Beige;
+            this.submitReviewButton.Location = new System.Drawing.Point(532, 431);
+            this.submitReviewButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.submitReviewButton.Name = "submitReviewButton";
+            this.submitReviewButton.Size = new System.Drawing.Size(184, 40);
+            this.submitReviewButton.TabIndex = 12;
+            this.submitReviewButton.Text = "Submit";
+            this.submitReviewButton.UseVisualStyleBackColor = false;
+            this.submitReviewButton.Click += new System.EventHandler(this.submitReviewButton_Click);
+            // 
+            // CustomerFeedbackForm
+            // 
+            this.AcceptButton = this.feedbackButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(958, 755);
+            this.Controls.Add(this.submitReviewButton);
+            this.Controls.Add(this.star5);
+            this.Controls.Add(this.star4);
+            this.Controls.Add(this.star3);
+            this.Controls.Add(this.star2);
+            this.Controls.Add(this.star1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.commentsTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "CustomerFeedbackForm";
+            this.Text = "CustomerFeedbackForm";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button feedbackButton;
+        private System.Windows.Forms.Button promotionsButton;
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Button orderSummary;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox commentsTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton star1;
+        private System.Windows.Forms.RadioButton star2;
+        private System.Windows.Forms.RadioButton star3;
+        private System.Windows.Forms.RadioButton star4;
+        private System.Windows.Forms.RadioButton star5;
+        private System.Windows.Forms.Button submitReviewButton;
+    }
+}
